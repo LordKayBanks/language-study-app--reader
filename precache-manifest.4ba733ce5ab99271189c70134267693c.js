07341b78bb1f608865de5e1988174a3fc1893d6a
Kayode Ogidan <kaybanks.developer@gmail.com>
2022-12-07 17:05:35 +0100

    Updates

--- precache-manifest.4ba733ce5ab99271189c70134267693c.js
+++ precache-manifest.4ba733ce5ab99271189c70134267693c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "03f2dad690f32a135037fba2839e093b",
+    "revision": "902f7b8c9e06d517276b6e870a6f97c9",
     "url": "/language-study-app--reader/index.html"
   },
   {
-    "revision": "31a5e74e581a209fef25",
+    "revision": "ad54fbac68987a2af7ab",
     "url": "/language-study-app--reader/static/css/2.82f03f93.chunk.css"
   },
   {
-    "revision": "827a663d708ee3793fc7",
-    "url": "/language-study-app--reader/static/css/main.ca835271.chunk.css"
+    "revision": "76a83d2319ddc692f475",
+    "url": "/language-study-app--reader/static/css/main.bce6a41a.chunk.css"
   },
   {
-    "revision": "31a5e74e581a209fef25",
-    "url": "/language-study-app--reader/static/js/2.c83c786f.chunk.js"
+    "revision": "ad54fbac68987a2af7ab",
+    "url": "/language-study-app--reader/static/js/2.ae4218c2.chunk.js"
   },
   {
     "revision": "f032203ca460334c00de541c30a6078a",
-    "url": "/language-study-app--reader/static/js/2.c83c786f.chunk.js.LICENSE"
+    "url": "/language-study-app--reader/static/js/2.ae4218c2.chunk.js.LICENSE"
   },
   {
-    "revision": "827a663d708ee3793fc7",
-    "url": "/language-study-app--reader/static/js/main.47a05389.chunk.js"
+    "revision": "76a83d2319ddc692f475",
+    "url": "/language-study-app--reader/static/js/main.5f942825.chunk.js"
   },
   {
     "revision": "3f7cacc9213404dc40e9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/language-study-app--reader/precache-manifest.869efade123642edc38c426f2da9770b.js"
+  "/language-study-app--reader/precache-manifest.4ba733ce5ab99271189c70134267693c.js"
 );
 
 self.addEventListener('message', (event) => {
